939483f31ee4bdbaccb75c89ae374c8308bac4d7
goggxi <goggxi@gmail.com>
2021-07-06 17:02:38 +0800

    singleton design patterns

---
+++ src/test/java/com/goggxi/springcreational/SpringDesignPatternsCreationalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goggxi.springcreational;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternsCreationalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
